6b7745e1eb72c419e69c47924681a1265a4e24b2
Simeon Tsekov <simeon.1.tsekov@gmail.com>
2020-12-30 11:11:23 +0200

    SP: Added 2 provinces in db and started working on getting data from the db.

---
+++ Warlords of Indochina/Assets/DbController.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Mono.Data.Sqlite;
+using System.Data;
+using System;
+using GlobalDatas;
+
+public class DbController : MonoBehaviour
+{
+    public static DbController Instance { get; private set; }
+    string conn;
+    IDbConnection dbconn;
+    IDbCommand dbcmd;
+    IDataReader reader;
+    string sqlQuery;
+
+    void Start()
+    {
+        if(Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        conn = "URI=file:" + Application.dataPath + "/Database.db";
+        dbconn = (IDbConnection)new SqliteConnection(conn);
+        dbconn.Open();
+        dbcmd = dbconn.CreateCommand();
+
+    }
+
+    ProvinceData GetProvinceInfo(string name)
+    {
+        sqlQuery = "SELECT p.Name, p.BuildingSlots, p.AvailableBuildingSlots, n.Color, t.Terrain, t.Attrition, t.DeffenderBonus" 
+            + "FROM Provinces p WHERE p.Name = " + name
+            + "INNER JOIN Nations n ON p.NationId = n.NationId"
+            + "INNER JOIN Terrain t ON p.TerrainId = t.TerrainId";
+        dbcmd.CommandText = sqlQuery;
+        reader = dbcmd.ExecuteReader;
+
+        while (reader.Read())
+        {
+
+        }
+        reader.Close();
+        reader = null;
+        dbcmd.Dispose();
+        dbcmd = null;
+        dbconn.Close();
+        dbconn = null;
+    }
+}
---
+++ Warlords of Indochina/Assets/Scripts/Provinces/ProvinceData.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace GlobalDatas
+{
+    public class ProvinceData
+    {
+        public string name;
+        public int buildingSlots;
+        public int availableBuildingSlots;
+        public string color;
+        public string terrain;
+        public int attrition;
+        public int deffenderBonus;
+
+        public ProvinceData(string name
+            , int buildingSlots
+            , int availableBuildingSlots
+            , string color
+            , string terrain
+            , int attrition
+            , int deffenderBonus)
+        {
+            this.name = name;
+            this.buildingSlots = buildingSlots;
+            this.availableBuildingSlots = availableBuildingSlots;
+            this.color = color;
+            this.terrain = terrain;
+            this.attrition = attrition;
+            this.deffenderBonus = deffenderBonus;
+        }
+    }
+}
