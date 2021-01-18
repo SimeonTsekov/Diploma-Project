bd8255e6d74fdb6a1666477cebc138e91d0afa37
Simeon Tsekov <simeon.1.tsekov@gmail.com>
2021-01-18 18:21:06 +0200

    SP: Province resources info now displayed in panel.

---
+++ Warlords of Indochina/Assets/Scripts/UI/ProvinceMenu/ProvinceGoldLabelController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace UI.ProvinceMenu
+{
+    public class ProvinceGoldLabelController : MonoBehaviour
+    {
+        private Text _txt;
+
+        private void Start()
+        {
+            _txt = GetComponent<Text>();
+            _txt.text = "";
+        }
+
+        private void Update()
+        {
+            try
+            {
+                _txt.text = "Local Manpower: " + ProvinceMenuController.Instance.ProvinceData.Manpower;
+            }
+            catch (Exception e)
+            {
+                Debug.Log(e);
+            }
+        }
+    }
+}
---
+++ Warlords of Indochina/Assets/Scripts/UI/ProvinceMenu/ProvinceManpowerLabelController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace UI.ProvinceMenu
+{
+    public class ProvinceManpowerLabelController : MonoBehaviour
+    {
+        private Text _txt;
+
+        private void Start()
+        {
+            _txt = GetComponent<Text>();
+            _txt.text = "";
+        }
+
+        private void Update()
+        {
+            try
+            {
+                _txt.text = "Local Gold: " + ProvinceMenuController.Instance.ProvinceData.Gold;
+            }
+            catch (Exception e)
+            {
+                Debug.Log(e);
+            }
+        }
+    }
+}
