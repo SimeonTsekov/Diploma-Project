c6df392666e54aa2cbbfb555d3564fce1c29efa3
Simeon Tsekov <simeon.1.tsekov@gmail.com>
2021-02-24 14:18:15 +0200

    SS: Added settings UI and began working on saves.

---
+++ Warlords of Indochina/Assets/Scripts/Saves/SaveData.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using Economy;
+using GlobalDatas;
+using UnityEngine;
+
+namespace Saves
+{
+	public class SaveData : MonoBehaviour
+	{
+		public List<ProvinceData> ProvinceDatas;
+		public List<BuildingManagement> BuildingManagements;
+
+	}
+}
--- Warlords of Indochina/Assets/Scripts/UI/UIController.cs
+++ Warlords of Indochina/Assets/Scripts/UI/UIController.cs
@@ -8,5 +8,6 @@ namespace UI
         {
             DontDestroyOnLoad(gameObject);
         }
+        
     }
 }
